b23ceb70648afdd0366a94697e7b007684a3c4d4
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2022-10-11 13:11:06 +0000

    use log folder

--- scripts/extract_torrents.sh
+++ scripts/extract_torrents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$TR_TORRENT_NAME is being extracted" >> /config/script.log
+echo "$TR_TORRENT_NAME is being extracted" >> /config/logs/script.log
 cd ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}
 unrar e -o- *.rar
-echo "extracted to ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" >> /config/script.log
+echo "extracted to ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" >> /config/logs/script.log
